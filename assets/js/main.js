fdea65377575e8e0927d5688a15faba16cac8ea4
Alex Carrera <alexcarrera84@gmail.com>
2022-12-07 01:02:28 -0300

    final ajust favicon text ajust and js fixed done and finish

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,42 +3,18 @@ $(document).scroll(function () {
     const y = $("html").scrollTop();
 
     y > 500 ?
-    $("nav").addClass("nav-black")
-    :
-    $("nav").removeClass("nav-black");
+        $("nav").addClass("nav-black")
+        :
+        $("nav").removeClass("nav-black");
 })
 
+//contactos//
+$("#enviar").click(function () {
+    alert("¡Gracias por escribir! Todo fue enviado correctamente.");
+});
+
+//footer - tooltip//
 $(function () {
     $('[data-bs-toggle="tooltip"]').tooltip();
-
-    // click boton enviar
-    $("#enviar").click(function () {
-        alert("El formulario fue enviado :)");
-    });
 })
 
-//Contactos//
-$("#nombre").click(function () {
-    var nombre = prompt("Ingrese su nombre y apellido", "");
-    var element = document.getElementById("nombre")
-    if (nombre !== "") {
-        element.value = nombre
-    }
-});
-
-$("#asunto").click(function () {
-    var tituloasunto = prompt("Ingrese el asunto", "");
-    var titulo = document.getElementById("asunto")
-    if (tituloasunto !== "") {
-        titulo.value = tituloasunto
-    }
-});
-
-
-$("#exampleFormControlTextarea1").click(function () {
-    var text = prompt("Ingrese  el texto del mensaje", "");
-    var element2 = document.getElementById("exampleFormControlTextarea1")
-    if (text !== "") {
-        element2.value = text
-    }
-});
